90d3d3b6d8c64d2bc5a7c9136c46a467caf86b11
Gonçalo Costa <goncalonuno_1995@hotmail.com>
2018-07-15 17:07:29 +0100

    login form

---
+++ login.php
@@ -0,0 +1,30 @@
+<link rel="stylesheet" href="style.css" type="text/css" media="screen">
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+
+
+<div class="login">
+	<!-- <img src="img/background.jpeg" class="login_image" alt="background"> -->
+	<div class="container">
+	    <div class="row">
+	        <div class="col-md-offset-4 col-md-4">
+	            <div class="form-login">
+	            <img src="img/logo.jpeg" alt="Logo">
+	            <input type="text" id="userName" class="form-control input-sm chat-input" placeholder="nome de utilizador" />
+	            </br>
+	            <input type="password" id="userPassword" class="form-control input-sm chat-input" placeholder="password" />
+	            </br>
+	            <div class="wrapper">
+	            <span class="group-btn">     
+	                <a href="#" class="btn btn-primary btn-md">Entrar <i class="fa fa-sign-in"></i></a>
+	            </span>
+	            </div>
+	            </div>
+        
+	        </div>
+	    </div>
+	</div>
+</div>
