d6cc9a0dc8c18d3e3079e50343b29b21a636672d
Gonçalo Costa <goncalonuno_1995@hotmail.com>
2018-07-18 20:55:02 +0100

    Preview photo addclient

--- scripting/clients.js
+++ scripting/clients.js
@@ -1,7 +1,9 @@
 
 	$(document).ready(function() {
+	   	//selecionar data
 	    $('#js-date').datepicker();
 		
+		//envia dados por ajax do formulario
 		$("form#clients-form").submit(function(e) {
 		    e.preventDefault();    
 		    var formData = new FormData(this);
@@ -30,4 +32,21 @@
 		        processData: false
 		    });
 		});
+		
+		//pre-visualizar foto
+	    function readURL(input) {
+	           if (input.files && input.files[0]) {
+	               var reader = new FileReader();
+
+	               reader.onload = function (e) {
+	                   $('#image_upload_preview').attr('src', e.target.result);
+	               }
+
+	               reader.readAsDataURL(input.files[0]);
+	           }
+	       }
+
+	       $("#photo").change(function () {
+	           readURL(this);
+	       });
 	});
