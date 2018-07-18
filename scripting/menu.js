cb364eaae6da63142376fe5343ecc8b8d71a5fb1
carlos goncalves <carlosecg31@hotmail.com>
2018-07-18 21:33:32 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ scripting/menu.js
@@ -0,0 +1,88 @@
+// Verifica a Hash
+
+  function checkLink()
+  {
+
+     var urlHash = window.location.hash;
+     var url_nonHash = urlHash.replace('#', '');
+     var functions = url_nonHash.split('&');
+     var toDo = [];
+
+     for (var i = 0; i < functions.length; i++)
+     {
+         var tempFunction = functions[i];
+         var tempArray = tempFunction.split('=');
+
+
+           if(tempArray.length == 2)
+           {
+             var tempKey = tempArray[0].replace('=', '');
+             var tempValue = tempArray[1];
+             toDo[tempKey] = tempValue;
+
+           }
+
+     }
+       return toDo;
+
+  }
+
+  $('body').on('click', '.produto-box', function()
+  {
+
+    var pid = $(this).attr('prdid');
+    getProductHash(pid);
+
+
+  });
+
+// Hash Change Listener
+// PROTOCOLO = view = página pretendida
+
+function changedURL()
+{
+
+ var toDo = checkLink();
+ $('#main').fadeOut(300, function(){
+
+   switch (toDo['view'])
+   {
+
+      case 'addclient':
+      $('#content').load('views/cliente.html');
+      $('#main').fadeIn();
+      break;
+
+      case 'clients':
+
+      break;
+      case 'addappointment':
+
+      break;
+      case 'appointments':
+        $('#content').load('views/agenda.html');
+        $('#main').fadeIn();
+      break;
+
+   }
+
+
+ });
+
+
+}
+
+window.onhashchange = function()
+{
+ changedURL();
+
+}
+
+$('.link-url').on('click', function()
+{
+    var link = $(this).attr('hashlink');
+    window.location.hash = 'view=' + link;
+
+});
+
+changedURL();
